3b8d1479d141ea01ef3ec9aca5d6cc7223ba138e
Kawal819 <singhkawal365@gmail.com>
2022-09-27 15:09:38 +0530

    E-commerce website

---
+++ src/common/footer/Footer.jsx
@@ -0,0 +1,61 @@
+import React from "react"
+import "./style.css"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='container grid2'>
+          <div className='box'>
+            <h1>K4 Store</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat et lectus vel ut sollicitudin elit at amet.</p>
+            <div className='icon d_flex'>
+              <div className='img d_flex'>
+                <i class='fa-brands fa-google-play'></i>
+                <span>Google Play</span>
+              </div>
+              <div className='img d_flex'>
+                <i class='fa-brands fa-app-store-ios'></i>
+                <span>App Store</span>
+              </div>
+            </div>
+          </div>
+
+          <div className='box'>
+            <h2>About Us</h2>
+            <ul>
+              <li>Careers</li>
+              <li>Our Stores</li>
+              <li>Our Cares</li>
+              <li>Terms & Conditions</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h2>Customer Care</h2>
+            <ul>
+              <li>Help Center </li>
+              <li>How to Buy </li>
+              <li>Track Your Order </li>
+              <li>Corporate & Bulk Purchasing </li>
+              <li>Returns & Refunds </li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h2>Contact Us</h2>
+            <ul>
+              <li>Chitkara University, Rajpura, Punjab </li>
+              <li>kapil0359.be20@chitkara.edu.in</li>
+              <li>kawal0378.be20@chitkara.edu.in</li>
+              <li>keshav0379.be20@chitkara.edu.in</li>
+              <li>kritika0397.be20@chitkara.edu.in</li>
+              <li>Phone: +91 1123 456 780</li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/common/header/Head.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> +91 423456789</label>
+            <i className='fa fa-envelope'></i>
+            <label> kapil0359@chitkara.edu.in.com</label>
+          </div>
+          <div className='right row RText'>
+            <label> FAQ"s</label>
+            <label>ANY Help?</label>
+            
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
